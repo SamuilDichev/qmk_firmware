a53f54b73e99c68e4d4b595df900e654acb269ff
SamuilDichev <samkata00@abv.bg>
2023-02-02 01:34:35 +0000

    Add my keymap

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/dyils/keymap.c
@@ -0,0 +1,116 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE,
+    _SPEC, /* Special layer with F1-F24 keys, media keys, etc */
+    _RGB,
+};
+
+enum tap_dances {
+    TD_SPECIAL_KEY,
+};
+
+void SPECIAL_KEY(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:  // Capitalizes the next word only
+            caps_word_on();
+            break;
+        case 3:  // Toggle RGB layer
+            if (layer_state_is(_RGB)) {
+                layer_off(_RGB);
+            } else {
+                layer_on(_RGB);
+            }
+            break;
+        case 10:  // Reset the keyboard so it can be flashed
+            reset_keyboard();
+    }
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_SPECIAL_KEY] = ACTION_TAP_DANCE_FN(SPECIAL_KEY),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Qwerty
+    * ,------------------------------------------------------------------------------------------.
+    * | ` ~  |  1! |  2@ |  3# |  4$ |  5% |  6^ |  7& |  8* |  9( |  0) |  -  |  =  |   Bkspc   |
+    * |------------------------------------------------------------------------------------------+
+    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |     \   |
+    * |------------------------------------------------------------------------------------------+
+    * | MO(_SPEC)|  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+    * |------------------------------------------------------------------------------------------+
+    * | Shift      |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |TD(CWORD|RGB|RES)|
+    * |------------------------------------------------------------------------------------------+
+    * | LCTL  |  Cmd  |  Alt  |              Space                | Left | Down |  Up  |  Right  |
+    * `------------------------------------------------------------------------------------------'
+    */
+    [_BASE] = LAYOUT_60_ansi(
+       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+       MO(_SPEC), KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,          KC_ENT,
+       KC_LSFT,            KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,          TD(TD_SPECIAL_KEY),
+       KC_LCTL,   KC_LGUI, KC_LALT,                              KC_SPC,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+    /* Special
+    * ,------------------------------------------------------------------------------------------.
+    * | ESC  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+    * |------------------------------------------------------------------------------------------+
+    * | NumLock| F13 |     | F14 | F15 |     |     |     | Ins |     |PrtSc|     |     |         |
+    * |------------------------------------------------------------------------------------------+
+    * |          |     |     |     | F16 |W_OFF|     |     |KLock| ScL |     |     |             |
+    * |------------------------------------------------------------------------------------------+
+    * |            | F17 | F18 | Calc|     |     | N_K | Mute|     |     |     |                 |
+    * |------------------------------------------------------------------------------------------+
+    * |       |  W_ON |       |            Play/Pause             | HOME | PG_DN | PG_UP |  END  |
+    * `------------------------------------------------------------------------------------------'
+    */
+    [_SPEC] = LAYOUT_60_ansi(
+        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        KC_NUM,   KC_F13,  _______, KC_F14,  KC_F15,   _______,  _______, _______, KC_INS,  _______,  KC_PSCR, _______, _______, _______,
+        _______,  _______, _______, _______, KC_F16,   GUI_OFF,  _______, _______, QK_LOCK, KC_SCRL,  _______, _______,          _______,
+        _______,           KC_F17,  KC_F18,  KC_CALC,  _______,  _______, NK_TOGG, KC_MUTE, _______,  _______, _______,          _______,
+        _______,  GUI_ON,  _______,                              KC_MPLY,                             KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    ),
+    /* RGB
+    * ,------------------------------------------------------------------------------------------.
+    * |      |     |     |     |     |     |     |     |     |     |     | BR- | BR+ |           |
+    * |------------------------------------------------------------------------------------------+
+    * |        |     | SP+ |     |     | TOG |     |     |     |     |     |     |     |         |
+    * |------------------------------------------------------------------------------------------+
+    * |          |     | SP- |     |     |     |     |     |     |     |     |     |             |
+    * |------------------------------------------------------------------------------------------+
+    * |            |     |     |     |     |     |     |     |     |     |     |                 |
+    * |------------------------------------------------------------------------------------------+
+    * |       |       |       |                                   | HUE- |  SAT- |  SAT+ |  HUE+ |
+    * `------------------------------------------------------------------------------------------'
+    */
+    [_RGB] = LAYOUT_60_ansi(
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, RGB_VAD, RGB_VAI, XXXXXXX,
+        XXXXXXX,  XXXXXXX, RGB_SPI, XXXXXXX, XXXXXXX,  RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,  XXXXXXX, RGB_SPD, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,          XXXXXXX, 
+        XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD, RGB_MOD, XXXXXXX,          _______,
+        XXXXXXX,  XXXXXXX, XXXXXXX,                              XXXXXXX,                             RGB_HUD, RGB_SAD, RGB_SAI, RGB_HUI
+    )
+
+    // TEMPLATE
+    // LAYOUT_60_ansi(
+    //    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    //    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    //    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    //    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    //    _______, _______, _______,                            _______,                            _______, _______, _______, _______,
+    // ),
+};
+
+
+bool rgb_matrix_indicators_user(void) {
+    switch (get_highest_layer(layer_state)) {
+        case _SPEC:
+            rgb_matrix_set_color_all(0,255,0);
+            return false; // "false" doesn't give back control of the RGB to the standard animations
+    }
+    
+    return true; // Give RGB control to the pre-configured RGB modes/animations
+}
--- keyboards/dztech/dz60rgb_ansi/v2/rules.mk
+++ keyboards/dztech/dz60rgb_ansi/v2/rules.mk
@@ -9,17 +9,20 @@ LAYOUTS = 60_ansi
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes          # Mouse keys
+BOOTMAGIC_ENABLE = yes         # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no           # Mouse keys
 EXTRAKEY_ENABLE = yes          # Audio control and System control
 CONSOLE_ENABLE = no            # Console for debug
 COMMAND_ENABLE = no            # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = yes              # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
 AUDIO_ENABLE = no              # Audio output
 RGB_MATRIX_ENABLE = yes        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
 NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
+TAP_DANCE_ENABLE = yes         # Enable Tap Dancing
+CAPS_WORD_ENABLE = yes         # Capitalizes a whole word
+KEY_LOCK_ENABLE = yes          # Locks a key down, see QK_LOCK
 
 LTO_ENABLE = yes
